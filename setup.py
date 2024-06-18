e29639b1c351fff34e89a6abda94e96b02ce71bc
Patrick Schröder <patrickschroder479@gmail.com>
2024-06-18 14:55:17 +0200

    Added layout

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """Main file, creates object of Qt interface, QApplication, QMainWindow, then calls methods to run the application."""
 
-from Interface.UI import Fractals_Mainwindow as f
+from Fractal_Art.Interface.UI import Fractals_Mainwindow as f
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
